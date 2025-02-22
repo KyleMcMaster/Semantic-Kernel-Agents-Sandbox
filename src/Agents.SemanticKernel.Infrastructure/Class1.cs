7df0709d2de817067b2187a69baf482a5d583c56
KyleMcMaster <11415127+KyleMcMaster@users.noreply.github.com>
2025-02-22 07:50:30 -0500

    Add solution, infrastructure, and test projects

---
+++ src/Agents.SemanticKernel.Infrastructure/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Agents.SemanticKernel.Infrastructure;
+
+public class Class1
+{
+
+}
---
+++ tests/Agents.SemanticKernel.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Agents.Sandbox.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
